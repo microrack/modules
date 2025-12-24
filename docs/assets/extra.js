dcdc8740066bbfe4dc59d947616965c4fc7dff10
MICRORACK <vco@microrack.org>
2025-12-25 02:54:00 +0400

    Change icon

--- docs/assets/extra.js
+++ docs/assets/extra.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', function() {
       link.textContent.trim() === 'Modules'
     );
     if (modulesTab) {
-      const icon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="14" height="14" style="vertical-align: middle; margin-right: 0.3rem;"><path fill="currentColor" d="M4 8h4V4H4v4zm6 12h4v-4h-4v4zm-6 0h4v-4H4v4zm0-6h4v-4H4v4zm6 0h4v-4h-4v4zm6-10v4h4V4h-4zm-6 4h4V4h-4v4zm6 6h4v-4h-4v4zm0 6h4v-4h-4v4z"/></svg>';
+      const icon = '<span style="font-size: 1.1rem; line-height: 1; vertical-align: middle; margin-right: 0.3rem;">⊶</span>';
       modulesTab.innerHTML = icon + 'Modules';
     }
 
